3b07b940d2c864ee12842667d578d838e5457f3d
KK <wkx611@outlook.com>
2023-07-22 23:13:18 +0800

    add new class VolumeImage and MedicalImageFolderMg

--- lib/medical_folder.py
+++ lib/medical_folder.py
@@ -1,55 +1,9 @@
 import SimpleITK as sitk
 from pathlib import Path
-from dataclasses import dataclass
-from typing import Sequence, Optional, Union, List
+from typing import Sequence, Optional
 from lib.folder import BaseMedicalImageFolderMg
 from lib.utility.define_class import STR_OR_PATH
-
-
-@dataclass
-class VolumeImage:
-    """Volume image information for fast and simple access
-
-    Attributes:
-        image (sitk.Image): sitk image
-        imageType (str): image type, e.g. MetaImage, NiftiImage, NrrdImage
-        dimension (int): image dimension, e.g. 2, 3
-        spacing (tuple): image spacing, e.g. (1.0, 1.0, 1.0)
-        origin (tuple): image origin, e.g. (0.0, 0.0, 0.0)
-        direction (tuple): image direction, e.g. (1.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-        width (int): image width, e.g. 512
-        height (int): image height, e.g. 512
-        depth (int): image depth, e.g. 512
-        pixelIDValue (int): pixel ID value, e.g. 8, 16, 32
-        pixelIDType (str): pixel ID type, e.g. scalar, vector, offset
-        pixelIDTypeAsString (str): pixel ID type as string, e.g. unsigned char, unsigned short, float
-
-    """
-
-    image: sitk.Image
-    dimension: int
-    spacing: tuple
-    origin: tuple
-    direction: tuple
-    width: int
-    height: int
-    depth: int
-    pixelIDValue: int
-    pixelIDType: str
-    pixelIDTypeAsString: str
-    necessaryTags: dict
-    necessaryTagsValue: list[str] = [
-        "0010|0010",  # Patient Name
-        "0010|0020",  # Patient ID
-        "0010|0030",  # Patient Birth Date
-        "0020|000D",  # Study Instance UID, for machine consumption
-        "0020|0010",  # Study ID, for human consumption
-        "0008|0008",  # Image Type
-        "0008|0020",  # Study Date
-        "0008|0030",  # Study Time
-        "0008|0050",  # Accession Number
-        "0008|0060",  # Modality
-    ]
+from lib.medical_image import VolumeImage
 
 
 class MedicalImageFolderMg(BaseMedicalImageFolderMg):
@@ -65,9 +19,27 @@ class MedicalImageFolderMg(BaseMedicalImageFolderMg):
 
     def __init__(self, folderFullPath: STR_OR_PATH):
         super().__init__(folderFullPath)
-        self.dicomSeries: Optional[Sequence[Path]] = None
+        self.dicomSeriesFolder: Optional[Sequence[Path]] = None
+        self.dicomSeries: Optional[Sequence[VolumeImage]] = None
+        self.findDicomSeriesFolder()
+        self.readAllDicomSeries()
 
-    def readDicomSeries(self, folderPath: STR_OR_PATH):
+    def readAllDicomSeries(self) -> Sequence[VolumeImage]:
+        """Read all dicom series from a folder
+
+        Returns:
+            Sequence[VolumeImage]: a list of dicom series
+        """
+        if self.dicomSeriesFolder is not None:
+            print("No dicom series found, please read dicom series first")
+            return []
+        print("read all dicom series")
+        for dicomFolder in self.dicomSeriesFolder:
+            self.dicomSeries.append(self.readDicomSeries(dicomFolder))
+            print(f"- {dicomFolder.name} read")
+        return [self.readDicomSeries(folder) for folder in self.dicomSeriesFolder]
+
+    def readDicomSeries(self, folderPath: STR_OR_PATH) -> VolumeImage:
         """Read dicom series from a folder
 
         Args:
@@ -87,41 +59,24 @@ class MedicalImageFolderMg(BaseMedicalImageFolderMg):
         series_reader.MetaDataDictionaryArrayUpdateOn()
         series_reader.LoadPrivateTagsOn()
         img = series_reader.Execute()
+        volumeImg = VolumeImage(img)
+        return volumeImg
 
-        return
+    def findDicomSeriesFolder(self):
+        if self.dicomSeriesFolder is None:
+            for folder in self.dirs:
+                if self._isADicomSeries(folder):
+                    print(f"- {folder.name} is a dicom series")
+                    self.dicomSeriesFolder.append(folder)
+                print(f"- {folder.name} is not a dicom series")
+        print(
+            f"found {len(self.dicomSeriesFolder)} dicom series already, skip this time"
+        )
 
-    def readSitkImageAndStoreInfo(
-        self, img: sitk.Image, printOut: bool = False
-    ) -> VolumeImage:
-        """Read sitk image and store image information"""
-        volumeImg = VolumeImage()
-        volumeImg.image = img
-        volumeImg.dimension = img.GetDimension()
-        volumeImg.spacing = img.GetSpacing()
-        volumeImg.origin = img.GetOrigin()
-        volumeImg.direction = img.GetDirection()
-        volumeImg.width = img.GetWidth()
-        volumeImg.height = img.GetHeight()
-        volumeImg.depth = img.GetDepth()
-        volumeImg.pixelIDValue = img.GetPixelIDValue()
-        volumeImg.pixelIDType = img.GetPixelIDType()
-        volumeImg.pixelIDTypeAsString = img.GetPixelIDTypeAsString()
-        for k in volumeImg.necessaryTagsValue:
-            volumeImg.necessaryTags[k] = img.GetMetaData(k)
-        if printOut:
-            print(f"Image Dimension: {volumeImg.dimension}")
-            print(f"Image Spacing: {volumeImg.spacing}")
-            print(f"Image Origin: {volumeImg.origin}")
-            print(f"Image Direction: {volumeImg.direction}")
-            print(f"Image Width: {volumeImg.width}")
-            print(f"Image Height: {volumeImg.height}")
-            print(f"Image Depth: {volumeImg.depth}")
-            print(f"Image PixelIDValue: {volumeImg.pixelIDValue}")
-            print(f"Image PixelIDType: {volumeImg.pixelIDType}")
-            print(f"Image PixelIDTypeAsString: {volumeImg.pixelIDTypeAsString}")
-            for k in volumeImg.necessaryTagsValue:
-                print(f"Image Necessary Tags {k}: {volumeImg.necessaryTags[k]}")
-        return img
+    def deleteDicomSeries(self):
+        if self.dicomSeriesFolder is not None:
+            self.dicomSeriesFolder = None
+            print("delete dicom series")
 
     def _isADicomSeries(self, folderPath: STR_OR_PATH) -> bool:
         series_IDs = sitk.ImageSeriesReader.GetGDCMSeriesIDs(str(folderPath))
